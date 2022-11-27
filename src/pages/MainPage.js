6913b351dd84c7ed8c2e8162cfb8a61e879121e4
sohyun <5399686@naver.com>
2022-11-28 02:17:59 +0900

    Implement: main page view

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import { PagesWrap } from "../components/Template";
+import { StartBtn, AccountBtn } from "../components/button/MainBtn";
+import img1 from "../images/main/main_img1.jpg";
 
 const MainPage = () => {
   return (
-  <div>
-    
-  </div>
-  )
+    <PagesWrap>
+      <Title>Sanlio keyring gamble</Title>
+      <img src={img1} alt='산리오x롯데제과 빼빼로 이미지' style={{ margin: "4rem" }} />
+      <StartBtn />
+      <AccountBtn />
+    </PagesWrap>
+  );
 };
+
+const Title = styled.h1`
+  font-size: 6rem;
+  color: #f277ab;
+  -webkit-text-stroke-width: 2px;
+  -webkit-text-stroke-color: #f01d76;
+`;
 
 export default MainPage;
