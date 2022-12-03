697d485713c8192fb74c2c72bb323e47f813554e
INSOCCI <5399686@naver.com>
2022-12-04 00:00:31 +0900

    Add gamepage

--- src/pages/game/Step1Page.js
+++ src/pages/game/Step1Page.js
@@ -1,7 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
 const Step1Page = () => {
-  return <div>Step1Page</div>;
+  return (
+  <Wrap>
+    <TitleBox>
+      <h2> [1단계] 은밀한 초콜릿 녹이기 </h2>
+      <p> Tip! 초콜릿 중탕에 좋은 온도는 45도입니다. </p>
+    </TitleBox>
+  </Wrap>
+  )
 };
+
+const Wrap = styled.div`
+  width: 100%;
+`;
+const TitleBox = styled.div`
+  width: 50%;
+  height: 200px;
+  h2{
+    font-size: 1.5rem;
+  }
+`;
 
 export default Step1Page;
